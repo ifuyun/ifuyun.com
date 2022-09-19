1be15614f013dc6c4530bc676d0c94176a125cc2
fuyun <ifuyun@hotmail.com>
2022-09-19 08:56:04 +0800

    fix: links error

--- src/app/components/toolbox/toolbox.component.ts
+++ src/app/components/toolbox/toolbox.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { isEmpty } from 'lodash';
+import { skipWhile, Subscription } from 'rxjs';
+import { OptionEntity } from '../../interfaces/options';
+import { OptionsService } from '../../services/options.service';
 
 @Component({
   selector: 'i-toolbox',
   templateUrl: './toolbox.component.html',
   styleUrls: ['./toolbox.component.less']
 })
-export class ToolboxComponent {}
+export class ToolboxComponent implements OnInit, OnDestroy {
+  options: OptionEntity = {};
+
+  private optionsListener!: Subscription;
+
+  constructor(private optionsService: OptionsService) {}
+
+  ngOnInit(): void {
+    this.optionsListener = this.optionsService.options$
+      .pipe(skipWhile((options) => isEmpty(options)))
+      .subscribe((options) => (this.options = options));
+  }
+
+  ngOnDestroy(): void {
+    this.optionsListener.unsubscribe();
+  }
+}
