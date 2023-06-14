78eece9c7305b43c1d9151b612739ff100fc46bd
fuyun <ifuyun@hotmail.com>
2023-06-15 06:42:33 +0800

    feature: show prompt source & author in prompt post

--- src/app/pages/prompt/prompt.interface.ts
+++ src/app/pages/prompt/prompt.interface.ts
@@ -1,11 +1,15 @@
+import { UserModel } from '../../interfaces/user.interface';
 import { PromptLang, PromptScope, PromptStatus } from './prompt.enum';
 
 export interface PromptEntity {
   promptId: string;
   promptTitle: string;
   promptContent: string;
+  promptOriginal: 0 | 1;
   promptLang: PromptLang;
   promptScope: PromptScope;
   promptStatus: PromptStatus;
   promptOrder: number;
+  promptMetaMap?: Record<string, string>;
+  user: UserModel;
 }
