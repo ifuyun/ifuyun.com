ec1df925d69b3b8d9c2685d7934f3a01c97adcd0
fuyun <ifuyun@hotmail.com>
2023-07-15 18:44:53 +0800

    fix: user router error

--- src/app/pages/user/user-routing.module.ts
+++ src/app/pages/user/user-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'confirm', component: ConfirmComponent },
-  { path: 'callback', component: ThirdLoginComponent }
+  { path: 'login/callback', component: ThirdLoginComponent }
 ];
 
 @NgModule({
