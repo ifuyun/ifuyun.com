71ed175b622de9ae80ea931c1ffe9c4d93b95d3d
fuyun <ifuyun@hotmail.com>
2025-05-27 21:56:51 +0800

    optimize deploy scripts

--- scripts/deploy-zip-all.sh
+++ scripts/deploy-zip-all.sh
@@ -7,20 +7,35 @@ echo -e "Starting deploy apps..."
 SHELL_PATH=$(dirname $0)
 cd $SHELL_PATH/..
 
-echo -e "Deploying www..."
-npm run deploy:zip:www
+# pull code
+echo -e "\033[95m[git]\033[0m Pulling from GitHub..."
+echo -e "\033[95m[git]\033[0m Path: \033[36m"$(pwd)"\033[0m"
+git fetch origin master && git reset --hard origin/master && git pull origin master
+git checkout master
+echo -e "\033[95m[git]\033[0m Pulled master."
+# set version
+VERSION=v$(npm pkg get version | tr -d '"')
 
-echo -e "Deploying blog..."
-npm run deploy:zip:blog
-
-echo -e "Deploying wallpaper..."
-npm run deploy:zip:wallpaper
-
-echo -e "Deploying jigsaw..."
-npm run deploy:zip:jigsaw
-
-echo -e "Deploying game..."
-npm run deploy:zip:game
+APPS=("www" "blog" "wallpaper" "jigsaw" "game")
+for APP_NAME in "${APPS[@]}"; do
+  PM2_APP_NAME="${APP_NAME}.ifuyun.com"
+  ZIP_FILENAME="release_${APP_NAME}_${VERSION}.zip"
+  # stop server
+  echo -e "\033[95m[server]\033[0m Stopping \033[36m${PM2_APP_NAME}\033[0m..."
+  pm2 stop $PM2_APP_NAME -s
+  echo -e "\033[95m[server]\033[0m \033[36m${PM2_APP_NAME}\033[0m is stopped."
+  # unzip file
+  echo -e "\033[95m[unzip]\033[0m Unzipping \033[36m${ZIP_FILENAME}\033[0m..."
+  unzip -q $ZIP_FILENAME -d release/$APP_NAME
+  rm -rf dist/$APP_NAME
+  mv release/$APP_NAME dist/$APP_NAME
+  rm -f $ZIP_FILENAME
+  echo -e "\033[95m[unzip]\033[0m Unzipped \033[36m${ZIP_FILENAME}\033[0m."
+  # restart server
+  echo -e "\033[95m[server]\033[0m Restarting \033[36m${PM2_APP_NAME}\033[0m..."
+  pm2 restart $PM2_APP_NAME
+  echo -e "\033[95m[server]\033[0m \033[36m${PM2_APP_NAME}\033[0m is online."
+done
 
 echo -e "All done. ^_-"
 
--- scripts/deploy-zip.sh
+++ scripts/deploy-zip.sh
@@ -21,9 +21,10 @@ if [ -z "$APP_NAME" ]; then
 fi
 
 PM2_APP_NAME="${APP_NAME}.ifuyun.com"
-SHELL_PATH=$(dirname $0)
 
+SHELL_PATH=$(dirname $0)
 cd $SHELL_PATH/..
+
 # pull code
 echo -e "\033[95m[git]\033[0m Pulling from GitHub..."
 echo -e "\033[95m[git]\033[0m Path: \033[36m"$(pwd)"\033[0m"
@@ -34,19 +35,19 @@ echo -e "\033[95m[git]\033[0m Pulled master."
 VERSION=v$(npm pkg get version | tr -d '"')
 ZIP_FILENAME="release_${APP_NAME}_${VERSION}.zip"
 # stop server
-echo -e "\033[95m[server]\033[0m Stopping server..."
+echo -e "\033[95m[server]\033[0m Stopping \033[36m${PM2_APP_NAME}\033[0m..."
 pm2 stop $PM2_APP_NAME -s
-echo -e "\033[95m[server]\033[0m Server: \033[36m${PM2_APP_NAME}\033[0m is stopped."
+echo -e "\033[95m[server]\033[0m \033[36m${PM2_APP_NAME}\033[0m is stopped."
 # unzip file
-echo -e "\033[95m[unzip]\033[0m Unzipping..."
+echo -e "\033[95m[unzip]\033[0m Unzipping \033[36m${ZIP_FILENAME}\033[0m..."
 unzip -q $ZIP_FILENAME -d release/$APP_NAME
 rm -rf dist/$APP_NAME
 mv release/$APP_NAME dist/$APP_NAME
 rm -f $ZIP_FILENAME
-echo -e "\033[95m[unzip]\033[0m Unzipped."
+echo -e "\033[95m[unzip]\033[0m Unzipped \033[36m${ZIP_FILENAME}\033[0m."
 # restart server
-echo -e "\033[95m[server]\033[0m Restarting server..."
+echo -e "\033[95m[server]\033[0m Restarting \033[36m${PM2_APP_NAME}\033[0m..."
 pm2 restart $PM2_APP_NAME
-echo -e "\033[95m[server]\033[0m Server: \033[36m${PM2_APP_NAME}\033[0m is online."
+echo -e "\033[95m[server]\033[0m \033[36m${PM2_APP_NAME}\033[0m is online."
 
 echo -e "\033[95m[deploy]\033[0m \033[32mAll done. ^_-\033[0m"
